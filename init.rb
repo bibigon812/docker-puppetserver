46e36fc3894cf89a99cd73477325dc0fce025128
Dmitry Yakovlev <dyakovlev@spbtv.com>
2018-06-28 13:38:53 +0300

    Use YAML instead ERB

--- init.rb
+++ init.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-cache_dir             = ENV['CACHE_DIR']
-git_remote            = ENV['GIT_REMOTE']
-environments_base_dir = ENV['ENVIRONMENTS_BASE_DIR']
+require 'yaml'
 
-require 'erb'
+config = {
+  cachedir: ENV['CACHE_DIR'],
+  source: {
+    basedir: ENV['ENVIRONMENTS_BASE_DIR'],
+    remote: ENV['GIT_REMOTE'],
+  }
+}
 
-File.write(
-  "#{ENV['R10K_CONFIG_DIR']}/r10k.yaml",
-  ERB.new(File.read(ENV['R10K_CONFIG_TEMPLATE'])).result(binding)
-)
-
+File.write("#{ENV['R10K_CONFIG_DIR']}/r10k.yaml", config.to_yaml)
